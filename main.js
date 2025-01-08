1ec0d439ad0a840cd16ad3ff4d8ed51ba9b5bdb9
BIKASH-KUMAR-RAY <2106BikashRay@gmail.com>
2025-01-08 16:47:57 +0000

    sdkjhck.jl

---
+++ main.js
@@ -0,0 +1,15 @@
+let a1=[0,1,2,3,4]
+console.log(a1);
+console.log(typeof a1);
+a1.push(6);
+console.log(a1)
+const a2=a1.slice(0,4)
+console.log(a2)
+console.log(a1);
+const a3=a1.splice(0,4)
+console.log(a3)
+console.log(a1)
+
+
+
+
